4f08ad747148a1cc62d4d039e5487200efb9959a
Ryouyaa <adhitya.aryo.aa@gmail.com>
2023-06-09 04:02:10 +0700

    Bootstrap 5 theme added

---
+++ resources/views/layout/Mainlayout.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+<head>
+
+@include('layout.partials.head')
+
+</head>
+
+
+<body>
+
+<div class="container-scroller">
+
+  @include('layout.partials.nav')
+
+  <div class="container-fluid page-body-wrapper">
+
+    @include('layout.partials.sidebar')
+
+    <div class="main-panel">
+
+      @yield('content-wrapper')
+
+      <!-- content-wrapper ends -->
+
+    </div>
+
+    <!-- main-panel ends -->
+
+  </div>
+
+  <!-- page-body-wrapper ends -->
+
+</div>
+
+<!-- container-scroller -->
+
+@include('layout.partials.footer-scripts')
+
+</body>
+
+</html>
